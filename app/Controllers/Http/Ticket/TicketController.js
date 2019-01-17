b5b44c6a6e0151e510152f5e43fbb37ca8563553
omar <0m4R#17o8>
2019-01-17 16:56:37 +0100

    fixes

--- app/Controllers/Http/Ticket/TicketController.js
+++ app/Controllers/Http/Ticket/TicketController.js
@@ -159,7 +159,7 @@ class TicketController {
     return response.route('ticketsShow', { id: ticket.id })
   }
 
-  async changeRecipient({ params, request, session, response }) {
+  async changeRecipient({ params, auth, request, session, response }) {
     const ticket = await Ticket.find(params.id)
 
     ticket.recipient_id = request.input('recipient_id')
