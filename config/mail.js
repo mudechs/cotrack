9f72b65182997810190db709c9a9653568222190
omar <0m4R#17o8>
2019-01-21 22:17:42 +0100

    mail fixes

--- config/mail.js
+++ config/mail.js
@@ -25,9 +25,9 @@ module.exports = {
   smtp: {
     driver: 'smtp',
     pool: true,
-    port: 25,
+    port: Env.get('MAIL_PORT'),
     host: Env.get('MAIL_HOST'),
-    secure: false,
+    secure: true,
     auth: {
       user: Env.get('MAIL_USERNAME'),
       pass: Env.get('MAIL_PASSWORD')
