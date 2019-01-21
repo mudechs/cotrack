a4d7d7189d6377f88cc5a0c9b8491d75b63192ab
omar <0m4R#17o8>
2019-01-21 16:44:37 +0100

    fixes

--- resources/assets/scripts/app.js
+++ resources/assets/scripts/app.js
@@ -1,13 +1,11 @@
 import $ from 'jquery/dist/jquery';
 import dragula from 'dragula/dragula';
-import axios from 'axios/dist/axios';
 import 'bootstrap/dist/js/bootstrap.bundle';
 
 
 window.jQuery = $;
 window.$ = $;
 window.dragula = dragula;
-window.axios = axios;
 
 $(document).ready(function () {
   // Add loading icon on clicked submit buttons
