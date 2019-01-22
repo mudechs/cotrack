4351ce63c858710c64e9837f16ae462579ac7c83
omar <0m4R#17o8>
2019-01-22 21:10:53 +0100

    fixes

--- app/Controllers/Http/Ticket/TicketController.js
+++ app/Controllers/Http/Ticket/TicketController.js
@@ -122,7 +122,7 @@ class TicketController {
     }
 
     try {
-      // Sende eine Notifikation falls der Recipient NICHT der Author isgt
+      // Sende eine Notifikation falls der Recipient NICHT der Author ist
       if(ticket.recipient_id !== null && ticket.recipient_id != auth.user.id) {
         const recipient = await ticket.ticketRecipient().fetch()
 
