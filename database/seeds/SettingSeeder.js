d7fe02fe9f34ff1366e6dc0b2e2f9d4d2ad33cbc
omar <0m4R#17o8>
2019-02-05 00:14:00 +0100

    added seeder

---
+++ database/seeds/SettingSeeder.js
@@ -0,0 +1,24 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| GlobalSeeder
+|--------------------------------------------------------------------------
+|
+| Make use of the Factory instance to seed database with dummy data or
+| make use of Lucid models directly.
+|
+*/
+
+/** @type {import('@adonisjs/lucid/src/Factory')} */
+const Factory = use('Factory')
+
+class SettingSeeder {
+  async run () {
+    await Factory
+      .model('App/Models/Setting')
+      .create()
+  }
+}
+
+module.exports = SettingSeeder
