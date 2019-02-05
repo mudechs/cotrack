9689f2639be9ebd1e4af3c05f4974b945d2c7ec5
omar <0m4R#17o8>
2019-02-05 18:17:48 +0100

    fixes

--- app/Controllers/Http/DashboardController.js
+++ app/Controllers/Http/DashboardController.js
@@ -21,7 +21,8 @@ class DashboardController {
 
     const ticketsAssignedToOthers = await Ticket.query()
       .where('author_id', auth.user.id)
-      .whereNot('recipient_id', auth.user.id)
+      .where('recipient_id', null)
+      .orWhereNot('recipient_id', auth.user.id)
       .whereIn('status', customStatuses)
       .orderBy('created_at', 'desc')
       .with('project', (builder) => {
