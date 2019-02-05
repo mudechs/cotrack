aed71e6fe69008b73eabfd484a6b80c638ffa40c
omar <0m4R#17o8>
2019-02-05 14:07:41 +0100

    fixes

--- resources/assets/scripts/app.js
+++ resources/assets/scripts/app.js
@@ -1,54 +1,59 @@
-import $ from 'jquery/dist/jquery';
-import dragula from 'dragula/dragula';
-import 'bootstrap/dist/js/bootstrap.bundle';
-import 'datatables.net-bs4/js/dataTables.bootstrap4';
-import 'bootstrap-multiselect/dist/js/bootstrap-multiselect';
-import { finished } from 'stream';
+import $ from 'jquery/dist/jquery'
+import dragula from 'dragula/dragula'
+import 'bootstrap/dist/js/bootstrap.bundle'
+import 'datatables.net-bs4/js/dataTables.bootstrap4'
+import 'bootstrap-multiselect/dist/js/bootstrap-multiselect'
 
-window.jQuery = $;
-window.$ = $;
-window.dragula = dragula;
+window.jQuery = $
+window.$ = $
+window.dragula = dragula
 
 $.extend(true, $.fn.dataTable.defaults, {
   dom: "<'row'<'col-6 col-sm-6 col-md-6'f><'col-6 col-sm-6 col-md-6'l>>" +
     "<'row'<'col-sm-12'tr>>" +
     "<'row d-flex align-items-center'<'col-6'i><'col-6'p>>",
-  "initComplete": function() {
-    $('div.dataTables_length select').removeClass('custom-select custom-select-sm');
+  'initComplete': function() {
+    $('div.dataTables_length select').removeClass('custom-select custom-select-sm')
   },
   info: true,
   paging: true,
-  lengthMenu: [[10, 25, 50, -1], [10, 25, 50, "Alle"]],
+  lengthMenu: [[10, 25, 50, -1], [10, 25, 50, 'Alle']],
   language: {
-    "search": "<i class='fas fa-filter'></i> ",
-    "info": "_START_ &rarr; _END_ (_TOTAL_)",
-    "infoEmpty": "0 &rarr; 0 (0)",
-    "emptyTable": "Keine Daten vorhanden",
-    "lengthMenu": "_MENU_",
-    "paginate": {
-      "first": "<i class='fas fa-step-backward'></i>",
-      "last": "<i class='fas fa-step-forward'></i>",
-      "next": "<i class='fas fa-forward'></i>",
-      "previous": "<i class='fas fa-backward'></i>"
+    'search': "<i class='fas fa-filter'></i> ",
+    'info': '_START_ &rarr; _END_ (_TOTAL_)',
+    'infoEmpty': '0 &rarr; 0 (0)',
+    'emptyTable': 'Keine Daten vorhanden',
+    'lengthMenu': '_MENU_',
+    'paginate': {
+      'first': "<i class='fas fa-step-backward'></i>",
+      'last': "<i class='fas fa-step-forward'></i>",
+      'next': "<i class='fas fa-forward'></i>",
+      'previous': "<i class='fas fa-backward'></i>"
     },
   }
-});
+})
 
 $(document).ready(function () {
   $('.multiselect').multiselect({
     buttonWidth: '100%'
-  });
+  })
 
   // Add loading icon on clicked submit buttons
   $('button[type=submit]').on('click', function () {
-    $(this).html('<i class="fas fa-spinner fa-spin fa-fw"></i>');
-  });
-});
+    $(this).html('<i class="fas fa-spinner fa-spin fa-fw"></i>')
+  })
 
-$('.linkable').on('click', function(e) {
-  e.preventDefault();
+  $('.linkable tr').on('click', 'td:not(:has(a))', function(e) {
+    e.preventDefault()
 
-  const url = $(this).data('route');
+    const url = $(this).closest('tr').data('route')
 
-  window.location.href = url;
-});
+    console.log(url)
+
+    if (url) {
+      window.location.href = url
+    }
+
+    return false
+  })
+})
