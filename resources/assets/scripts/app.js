9542523e4cc09e644bbbe2f75f7e7e44aadaf0e3
omar <0m4R#17o8>
2019-02-13 15:24:21 +0100

    replaced multiselect

--- resources/assets/scripts/app.js
+++ resources/assets/scripts/app.js
@@ -1,18 +1,13 @@
 import $ from 'jquery/dist/jquery'
 import dragula from 'dragula/dragula'
 import 'bootstrap/dist/js/bootstrap.bundle'
-import 'bootstrap-multiselect/dist/js/bootstrap-multiselect'
-import axios from 'axios/dist/axios'
+import axios from 'axios'
 import helpers from './helpers'
+import choices from 'choices.js'
 
 window.jQuery = $
 window.$ = $
 window.dragula = dragula
 window.axios = axios
 window.helpers = helpers
-
-$(document).ready(function () {
-  $('.multiselect').multiselect({
-    buttonWidth: '100%'
-  })
-})
+window.choices = choices
