e3f50b839286ee5e8c0194001f3115a1690ab655
omar <0m4R#17o8>
2019-03-01 09:18:59 +0100

    changed basecolor

--- app/Services/projectServices.js
+++ app/Services/projectServices.js
@@ -4,17 +4,19 @@ const Project = use('App/Models/Project');
 const TicketServices = use('App/Services/ticketServices');
 
 class projectServices {
-  async getUserProjects(user, statusGroup, locale) {
+  async getUserProjects(userId, statusGroup, locale) {
     const statuses = await TicketServices.ticketStatuses(statusGroup, locale);
 
     return await Project.query()
       .where('is_active', true)
-      .where('author_id', user)
-      .orWhereHas('members', (builder) => {
-        builder.where('user_id', user);
+      .whereHas('tickets', (builder) => {
+        builder.whereIn('status', statuses).where('recipient_id', userId);
+      })
+      .whereHas('members', (builder) => {
+        builder.where('user_id', userId);
       })
       .withCount('tickets', (builder) => {
-        builder.whereIn('status', statuses).where('recipient_id', user);
+        builder.whereIn('status', statuses).where('recipient_id', userId);
       })
       .orderBy('title', 'asc')
       .fetch();
